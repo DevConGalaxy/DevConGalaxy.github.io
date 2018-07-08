e6d9ab5e9d2ae8e78ed254f59b9d7181a43f426e
Olivier <leplus.olivier@gmail.com>
2018-07-08 19:41:37 +0200

    feat(sort/filter): Add sort and filter options for tutorials
    fix #1
    fix #2

--- src/app/codelabs/codelabs.component.ts
+++ src/app/codelabs/codelabs.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { TutorialService } from '../tutorial.service';
+import { languages } from 'prismjs';
 
+export interface Tutorial {
+  id: string,
+  title: string,
+  duration: number,
+  language: string,
+  date: string
+};
 
 export interface CodelabInfos {
   title: string;
   headline: string,
   tagline: string,
-  codelabs: Array<any>
-}
+  tutorials: Array<Tutorial>
+};
 
 @Component({
   selector: 'app-codelabs',
@@ -17,21 +25,56 @@ export interface CodelabInfos {
 export class CodelabsComponent implements OnInit {
   public searchText: string;
   public infos: CodelabInfos;
+  public tutorials: Array<Tutorial>
 
   public sortingOptions: Array<any> = [
-    { value: 'alphabetical', viewValue: 'Alphabetical' },
+    { value: 'title', viewValue: 'Alphabetical' },
     { value: 'duration', viewValue: 'Duration' },
-    { value: 'recent', viewValue: 'Recent' }
+    { value: 'date', viewValue: 'Recent' }
   ];
 
   constructor(private ts: TutorialService) {
     this.ts.getCodelabs().subscribe((response: any) => {
       this.infos = response;
+      this.tutorials = response.tutorials;
     });
   }
 
-  filter(event) {
-    console.log(event);
+  sortByDate(tutorial1, tutorial2) {
+    const date1 = new Date(tutorial1.date);
+    const date2 = new Date(tutorial2.date);
+    if (date1 < date2) {
+      return 1;
+    }
+    if (date1 > date2) {
+      return -1;
+    }
+    return 0;
+  }
+
+  sortBy(event) {
+    this.tutorials = this.infos.tutorials.sort((tutorial1, tutorial2) => {
+      console.log(event);
+      if (event.value === "date") {
+        return this.sortByDate(tutorial1, tutorial2);
+      } else {
+        if (tutorial1[event.value] > tutorial2[event.value]) {
+          return 1;
+        }
+        if (tutorial1[event.value] < tutorial2[event.value]) {
+          return -1;
+        }
+        return 0;
+      }
+    });
+  }
+
+  filterByLanguage(event) {
+    if (event.value === "all") {
+      this.tutorials = this.infos.tutorials;
+    } else {
+      this.tutorials = this.infos.tutorials.filter(tutorial => tutorial.language === event.value);
+    }
   }
 
   ngOnInit() {
