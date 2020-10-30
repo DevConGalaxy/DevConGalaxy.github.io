6948d70c2279b666efb664a5a3460a512898c44e
vivienlonde <vivien.londe@gmail.com>
2020-10-30 09:27:23 +0100

    'ressources' fonctionne en local

--- notes.md
+++ notes.md
@@ -1,6 +1,10 @@
 # changement local/en ligne
 
-Dans le fichier tutorial.service.ts, dans la fonction getTutorialMd, il faut utiliser:
+Dans le fichier tutorial.service.ts, dans les fonctions getTutorialMd et getTutorialData, il faut utiliser:
 
 - la première ligne pour publier la page.
 - la deuxième ligne pour le développement en local.
+
+# Téléchargement
+
+Les liens de téléchargement ne fonctionnent pas en local.
--- src/app/tutorial.service.ts
+++ src/app/tutorial.service.ts
@@ -16,11 +16,11 @@ export class TutorialService {
 
   getTutorialMd(tutorialId: string) {
     // return this.http.get(`https://raw.githubusercontent.com/tagazok/workshops/gh-pages/assets/codelabs/${tutorialId}/${tutorialId}.md`, {responseType: 'text'});
-
     return this.http.get(`assets/codelabs/${tutorialId}/${tutorialId}.md`, {responseType: 'text'});
   }
 
   getTutorialData(tutorialId: string) {
-    return this.http.get(`https://raw.githubusercontent.com/tagazok/workshops/gh-pages/assets/codelabs/${tutorialId}/data.json`);
+    // return this.http.get(`https://raw.githubusercontent.com/tagazok/workshops/gh-pages/assets/codelabs/${tutorialId}/data.json`);
+    return this.http.get(`assets/codelabs/${tutorialId}/data.json`);
   }
 }
