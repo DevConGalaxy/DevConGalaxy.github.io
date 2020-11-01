1bf795247908c08f3e18cfdafdd3f76625ef8770
Olivier <leplus.olivier@gmail.com>
2020-11-01 16:35:36 +0100

    fix(naming): Fix naming for nex codelab convention
    Fix links to resources

--- src/app/tutorial.service.ts
+++ src/app/tutorial.service.ts
@@ -11,16 +11,16 @@ export class TutorialService {
   constructor(private http: HttpClient) { }
 
   getAllTutorials() {
-    return this.http.get(`assets/codelabs/tutorials.json`);
+    return this.http.get(`./assets/codelabs/tutorials.json`);
   }
 
   getTutorialMd(tutorialId: string) {
     // return this.http.get(`https://raw.githubusercontent.com/tagazok/workshops/gh-pages/assets/codelabs/${tutorialId}/${tutorialId}.md`, {responseType: 'text'});
-    return this.http.get(`assets/codelabs/${tutorialId}/${tutorialId}.md`, {responseType: 'text'});
+    return this.http.get(`./assets/codelabs/${tutorialId}/tutorial.md`, {responseType: 'text'});
   }
 
   getTutorialData(tutorialId: string) {
     // return this.http.get(`https://raw.githubusercontent.com/tagazok/workshops/gh-pages/assets/codelabs/${tutorialId}/data.json`);
-    return this.http.get(`assets/codelabs/${tutorialId}/data.json`);
+    return this.http.get(`./assets/codelabs/${tutorialId}/data.json`);
   }
 }
